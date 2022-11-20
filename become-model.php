b25f9562f7a1e23b9be3efbab1362a0a47d2bed2
Tom Kaufmann <t.kaufmann@svg.de>
2022-11-20 20:02:50 +0100

    added become-model.php

---
+++ become-model.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Model werden */ ?>
+<?php get_header(); ?>
+<div class="container-fluid mt-5 bg-black text-white">
+    <div class="row d-flex justify-content-center ">
+        <div class="col-6 p-5">
+         <h2 class="px-5 pt-5">Model werden</h2>
+         <p class="px-5 pt-5 font-20">Wir helfen dir dabei, den Weg in die Modelwelt zu finden und dich bei Kunden attraktiv zu präsentieren. Neben der Aufnahme als Newcomer Model in Modelagenturen, ist es wichtig als Model auch selbst aktiv nach individuellen Möglichkeiten zu suchen, an Jobs zu kommen, Durchhaltevermögen zu beweisen und in der Mode- und Werbewelt präsent zu sein. Bewirb dich bei uns und lerne mehr über das Modeln und wie du am besten durchstarten kannst.</p>
+        <a class="px-5 pt-5 font-20" href="idealimodels.de/bewerbung">Zur Bewerbung</a>
+        </div>
+        <div class="col-6 p-0">
+            <img src="<?php echo get_template_directory_uri()?>/img/banner/model-werden-banner-rechts.png"/>
+        </div>
+        <div class="
+    </div>
+</div>
+<?php get_footer(); ?>
