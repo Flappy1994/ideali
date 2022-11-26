fc2c95908bda2511790c28d900cab445c2368d98
Tom Kaufmann <t.kaufmann@svg.de>
2022-11-26 17:34:33 +0100

    wip

--- become-model.php
+++ become-model.php
@@ -1,11 +1,13 @@
 <?php /* Template Name: Model werden */ ?>
 <?php get_header(); ?>
-<div class="container-fluid mt-5 bg-black text-white">
-    <div class="row d-flex justify-content-center ">
+<div class="container-fluid bg-black text-white">
+    <div class="row d-flex justify-content-center">
         <div class="col-6 p-5">
          <h2 class="px-5 pt-5">Model werden</h2>
          <p class="px-5 pt-5 font-20">Wir helfen dir dabei, den Weg in die Modelwelt zu finden und dich bei Kunden attraktiv zu präsentieren. Neben der Aufnahme als Newcomer Model in Modelagenturen, ist es wichtig als Model auch selbst aktiv nach individuellen Möglichkeiten zu suchen, an Jobs zu kommen, Durchhaltevermögen zu beweisen und in der Mode- und Werbewelt präsent zu sein. Bewirb dich bei uns und lerne mehr über das Modeln und wie du am besten durchstarten kannst.</p>
-        <a class="px-5 pt-5 font-20" href="/bewerbung">Zur Bewerbung</a>
+            <div class="col-12 px-5">
+            <a class=" pt-5 font-20 text-white" href="/bewerbung">Zur Bewerbung</a>
+            </div>
         </div>
         <div class="col-6 p-0">
             <img src="<?php echo get_template_directory_uri()?>/img/banner/model-werden-banner-rechts.png"/>
@@ -68,7 +70,9 @@
         <div class="col-6 p-5 mt-lg-5">
          <h2 class="px-5 pt-5 mt-lg-5">Model werden</h2>
          <p class="px-5 pt-5 font-20">Schick uns noch heute deine Bewerbung zu. Fülle hierzu unser Registrierungsformular aus und sende uns einige Polaroids von dir. Wir freuen uns auf neue Gesichter.</p>
-        <a class="px-5 pt-5 font-20" href="/bewerbung">Zur Bewerbung</a>
+         <div class="col-12 px-5">
+                <a class="pt-5 font-20 text-white" href="/bewerbung">Zur Bewerbung</a>
+            </div>
         </div>
         <div class="col-6 p-0">
             <img class="w-100" src="<?php echo get_template_directory_uri()?>/img/polaroids/model-werden-banner-rechts-2.jpg"/>
